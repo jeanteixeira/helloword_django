db6804330556ec00fd093a10489c839737a262c9
Jean B <jean.barros.teixeira@gmail.com>
2020-08-14 11:30:54 -0300

    Add model Funcionario

--- helloworld/helloworld/models.py
+++ helloworld/helloworld/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Funcionario(models.Model):
+
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    lastName = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+
+    serviceTime = models.IntegerField(
+        default=0,
+        null=False,
+        blank=False
+    )
+
+    salary = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+
+objects = models.Manager()
